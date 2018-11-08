de10b0f9c75c72afbb1e96d818b43a6ac46eeecf
Christof Weickhardt <>
2018-11-08 09:51:52 +0100

    added task 1 for chapter 07

---
+++ 07-ps-skripts/07a1.ps1
@@ -0,0 +1,8 @@
+﻿param (
+    [Parameter(Mandatory=$true)][int] $zahl,
+    [Parameter(Mandatory=$true)][int] $delay
+)
+1..$zahl | ForEach-Object{
+    Start-Sleep -Milliseconds $delay
+    Write-Host $_
+}
