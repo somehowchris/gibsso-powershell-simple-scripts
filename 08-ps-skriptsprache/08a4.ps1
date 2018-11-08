c57dcb8b6409be6015767c45558f4bbb7176fe77
Nicola <blocktutorial.ch@gmail.com>
2018-11-08 14:58:24 +0100

    done a4

--- 08-ps-skriptsprache/08a4.ps1
+++ 08-ps-skriptsprache/08a4.ps1
@@ -1,13 +1,23 @@
 $proc = Get-Process
+$red = 0
+$green = 0
+$white = 0
 
 foreach($pr in $proc)
 {
     if($pr.cpu -gt 1000)
     {
         write-host -ForegroundColor Green $pr.Id, $pr.Name
+        $green++
     }elseif($pr.cpu -ge 300){
         write-host -ForegroundColor White $pr.Id, $pr.Name
+        $white++
     }else{
         write-host -ForegroundColor red $pr.Id, $pr.Name
+        $red++
     }
 }
+ write-host "Red: $red"
+ write-host "White: $white"
+ write-host "Green: $green"
+
